dc888bca59f8cdc7e38266aa49deb9294bb8ace9
mpellouin <maxence.pellouin@epitech.eu>
2022-09-19 23:22:00 +0200

    add: about.json type

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AboutType } from './types/about';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("about.json")
+  getAboutJson(): AboutType {
+    return this.appService.getAboutJson();
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { AboutType } from './types/about';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getAboutJson(): AboutType {
+    return {
+      client: {
+        host: "localhost",
+      },
+      server: {
+        current_time: 0,
+        services: [
+          {
+            name: "service example",
+            actions: [
+              {
+                name: "action1",
+                description: "action1 description",
+              },
+            ],
+            reactions: [
+              {
+                name: "reaction1",
+                description: "reaction1 description",
+              },
+            ],
+          },
+          {
+            name: "service2",
+            actions: [
+              {
+                name: "action1",
+                description: "action1 description",
+              },
+            ],
+            reactions: [
+              {
+                name: "reaction1",
+                description: "reaction1 description",
+              },
+            ],
+          },
+        ],
+      },
+    };
   }
 }
