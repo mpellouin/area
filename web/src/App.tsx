e2421668a79e3586bdb3d5c332a39e631bdda4b0
Justine <justine.trupheme@epitech.eu>
2022-09-22 15:02:45 +0200

    add: header done and routing systeme

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* <Route path="/" element={<Header />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import '../scss/header.scss';
 
-
 function Header() {
+    const [headerBackground, setHeaderBackground] = useState(true);
+    const changeBackground = () => {
+        if (window.scrollY > 0) 
+            setHeaderBackground(false);
+        else
+            setHeaderBackground(true);
+        }
+    useEffect(() => {
+        window.addEventListener('scroll', changeBackground);
+        return () => {
+            window.removeEventListener('scroll', changeBackground);
+        }
+    }, []);
     return (
-        <p>Header</p>
+        <header className={headerBackground ? 'header' : 'header-pinned'}>
+            <div className='logo'>
+                <Link to='/'>
+                    <img src={require('../assets/Logo.svg').default} alt='Logo' width='100' height='100'/>
+                </Link>
+            </div>
+            <div className='navbar'>
+                <Link className='navbar-item' to='/'>
+                    HOME
+                </Link>
+                <Link className='navbar-item' to='/auth'>
+                    <button className='navbar-button'>LOGIN</button>
+                </Link>
+            </div>
+        </header>
     );
 }
 
