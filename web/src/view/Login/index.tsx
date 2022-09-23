92a4459240e69f90ae06a520f327cc4abc3e2588
mpellouin <maxence.pellouin@epitech.eu>
2022-09-23 15:07:32 +0200

    add: login back svgs and correct form

--- web/src/view/Login/index.tsx
+++ web/src/view/Login/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import './index.scss'
 
 const Login = () => {
@@ -9,20 +10,33 @@ const Login = () => {
     return (
         <>
             <div className="page">
+                <img className="loginBack" src="LoginBack.svg" alt="loginBack" />
+                <img className="loginBackFiles" src="LoginBackFiles.svg" alt="loginBackRight" />
                 <div className="formContainer">
                     <div className="form">
                         <div className="formHeader">
                             <h1>LOG IN</h1>
                         </div>
                         <div className="formBody">
-                                <input type="email" name="email" id="email" placeholder="Enter Email"/>
-                                <input type={passIsShown ? 'text' : 'password'} name="password" id="passwordInput" placeholder="Enter Password"/>
-                                <label htmlFor="passwordInput" onClick={() => setPassword('')}><b>Reset password</b></label>
+                                <input type="email" name="email" id="email" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                <input type={passIsShown ? 'text' : 'password'} name="password" id="passwordInput" placeholder="Enter Password" unselectable="on" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                <label htmlFor="passwordInput" onClick={() => console.log("LIsa")}><b>Forgot password?</b></label>
                         </div>
                         <div className="formFooter">
                             <button>Sign In</button>
                         </div>
-                        <p className="registerText">Don't have an account? <b>Register now!</b></p>
+                        <hr className="lineText" data-content="Or sign with"/>
+                        <div className="alternateLogins">
+                            <button>
+                                <img src="logo_google.svg" alt="Google" />
+                            </button>
+                            <button className="facebookLogin">
+                                <img src="logo_apple.svg" alt="Apple" />
+                            </button>
+                        </div>
+                        <Link className='registerText' to='/login'>
+                            <p>Don't have an account? <b>Register now!</b></p>
+                        </Link>
                     </div>
                 </div>
             </div>
