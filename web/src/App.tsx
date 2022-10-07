8aa1bf2e362ef955f42481de1a1a5748b594859a
Florian <florian.gibault@epitech.eu>
2022-10-07 12:23:45 +0200

    add: homepage main structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Login from "./view/Login";
 import Register from "./view/Register";
 import Footer from './components/Footer';
+import Home from "./view/Home";
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
