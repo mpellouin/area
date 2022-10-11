8cd08255ff3b62486327dc17cb543d6c4d233d4b
Laurent-cazette <Laurent.cazette@epitech.eu>
2022-10-11 16:47:59 +0200

    changed port setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // Check why GET / seem to be not working on 8080
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
