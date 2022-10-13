3c77ac8285ae2227f617e364a26ff514a26cfe13
mpellouin <maxence.pellouin@epitech.eu>
2022-10-13 16:41:46 +0200

    refacto: code structure for actions and reactions, and created some subservices

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -10,13 +10,17 @@ import { AuthService } from './Oauth/auth.service';
 import { AuthController } from './Oauth/auth.controller';
 import { HttpModule } from '@nestjs/axios';
 import { ActionsService } from './actions/actions.service';
-import { ReactionController } from './reactions/reaction.controller';
 import { ReactionService } from './reactions/reaction.strategy';
 import { ProviderService } from './providers/provider.service';
+import { TwitterActionsService } from "./actions/twitter/twitter.actions.service";
+import { GoogleReactionsService } from "./reactions/google/google.reactions.service";
 
 @Module({
   imports: [ConfigModule.forRoot(), HttpModule],
-  controllers: [AppController, userController, AuthController, ReactionController],
-  providers: [AppService, UserService, PrismaService, GoogleStrategy, AuthService, ReactionService, ActionsService, ProviderService],
+  controllers: [AppController, userController, AuthController],
+  providers: [AppService, UserService, PrismaService,
+              GoogleStrategy, AuthService, ReactionService,
+              ActionsService, ProviderService, TwitterActionsService,
+              GoogleReactionsService],
 })
 export class AppModule {}
