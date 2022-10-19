3d14a1012b8277b23a7e16cb2f4d92b6143ac7f9
Laurent-cazette <Laurent.cazette@epitech.eu>
2022-10-19 20:55:37 +0200

    added auto env

---
+++ server/src/Oauth/oauth.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Redirect, Req, Res, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { response } from "express";
+import { OAuthService } from "./oauth.service";
+
+@Controller("auth")
+export class OAuthController {
+    constructor (private authService: OAuthService) {}
+
+    @UseGuards(AuthGuard("google"))
+    @Get("google")
+    async loginWithGoogle() {
+        console.log("someone is trying to login with google")
+    }
+
+    @UseGuards(AuthGuard("google"))
+    @Get("google/redirect")
+    async loginWithGoogleRedirect(@Req() req, @Res() res, @Body() body?: {email: string}) {
+        const user = await this.authService.loggingWithGoogle(req, body)
+        if (user) {
+            res.redirect("http://localhost:8081/Areas?token=" + user.accessToken )
+        }
+    }
+}
