d834340771768703955812cdda61fb8f60c59ddb
Laurent-cazette <69351870+Laurent-cazette@users.noreply.github.com>
2022-10-25 13:29:26 +0400

    Delete utilities.tsx

--- mobile/src/components/utilities.tsx
+++
@@ -1,7 +0,0 @@
-import { Platform } from 'react-native'
-export const getDeepLink = (path = '') => {
-  const scheme = 'http'
-  const host = "https://lisolescargot.netlify.app"
-  const prefix = `${scheme}://host/`
-  return prefix + path
-}
