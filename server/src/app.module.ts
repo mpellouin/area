e9c07a21b8bfe12e508517f124ed7e13082a033b
Laurent-cazette <Laurent.cazette@epitech.eu>
2022-10-26 16:22:35 +0200

    fixed server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { userController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { PrismaService } from './prisma.service';
 import { GoogleStrategy } from './Oauth/google.strategy';
