3678742af1bb9915e2ffb10f9e1ff71ba172b95a
Laurent-cazette <Laurent.cazette@epitech.eu>
2022-10-27 15:46:42 +0200

    corrected little error on area service

--- server/src/area/area.module.ts
+++ server/src/area/area.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
 import { AreaService } from "./area.service";
 
 @Module({
     controllers: [],
-    providers: [AreaService],
+    providers: [AreaService, PrismaService],
     exports: [AreaService]
 })
 export class AreaModule {}
