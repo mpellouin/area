ba65d7b2e414f9efc0295e65c4edfee122055f19
mpellouin <maxence.pellouin@epitech.eu>
2022-10-28 17:00:21 +0200

    add: services subscription and unsubscription

--- server/src/services/services.service.ts
+++ server/src/services/services.service.ts
@@ -1,67 +1,78 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
-import { Service, Prisma } from '@prisma/client';
+import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
+import {PrismaService} from '../prisma.service';
+import {Service, Prisma, User} from '@prisma/client';
 
 @Injectable()
 export class ServicesService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {}
 
-  async findMany(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.ServiceWhereUniqueInput;
-    where?: Prisma.ServiceWhereInput;
-    orderBy?: Prisma.ServiceOrderByWithRelationInput;
-  }): Promise<Service[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.service.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
-    });
-  }
-
-  async createService(data: Prisma.ServiceCreateInput): Promise<Service> {
-    return this.prisma.service.create({
-      data,
-    });
-  }
-
-  async updateService(params: {
-    where: Prisma.ServiceWhereUniqueInput;
-    data: Prisma.ServiceUpdateInput;
-  }): Promise<Service> {
-    const { where, data } = params;
-    return this.prisma.service.update({
-      data,
-      where,
-    });
-  }
-
-  async deleteService(where: Prisma.ServiceWhereUniqueInput): Promise<Service> {
-    return this.prisma.service.delete({
-      where,
-    });
-  }
-
-  async subscribe(serviceId: number, req: any): Promise<Service> {
-    const service = await this.prisma.service.findUnique({
-      where: { ID: serviceId },
-    });
-    if (!service) {
-      throw new Error('Service not found');
+    async findMany(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.ServiceWhereUniqueInput;
+        where?: Prisma.ServiceWhereInput;
+        orderBy?: Prisma.ServiceOrderByWithRelationInput;
+    }): Promise<Service[]> {
+        const {skip, take, cursor, where, orderBy} = params;
+        return this.prisma.service.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+        });
     }
-    const user = await this.prisma.user.findUnique({
-      where: { ID: req.user.ID },
-    });
-    if (!user) {
-      throw new Error('User not found');
+
+    async createService(data: Prisma.ServiceCreateInput): Promise<Service> {
+        return this.prisma.service.create({
+            data,
+        });
     }
-    if (await this.prisma.service.findMany({where: {ID: serviceId}})) {
-      throw new Error('Service not found');
+
+    async updateService(params: {where: Prisma.ServiceWhereUniqueInput; data: Prisma.ServiceUpdateInput}): Promise<Service> {
+        const {where, data} = params;
+        return this.prisma.service.update({
+            data,
+            where,
+        });
     }
-    return 
-  }
+
+    async deleteService(where: Prisma.ServiceWhereUniqueInput): Promise<Service> {
+        return this.prisma.service.delete({
+            where,
+        });
+    }
+
+    async subscribe(serviceId: number, req: any): Promise<User> {
+        const user = await this.prisma.user.findUnique({
+            where: {ID: req.user.ID},
+        });
+        console.log(req.user, user, serviceId);
+        if (user.services.includes(serviceId.toString())) {
+            return;
+        } else {
+            return this.prisma.user.update({
+                where: {ID: req.user.ID},
+                data: {
+                    services: user.services + serviceId.toString(),
+                },
+            });
+        }
+    }
+
+    async unsubscribe(serviceId: number, req: any): Promise<User> {
+        const user = await this.prisma.user.findUnique({
+            where: {ID: req.user.ID},
+        });
+        if (!user.services.includes(serviceId.toString())) {
+            return;
+        } else {
+            return this.prisma.user.update({
+                where: {ID: req.user.ID},
+                data: {
+                    services: user.services.split(serviceId.toString()[0]).join(''),
+                },
+            });
+        }
+    }
 }
