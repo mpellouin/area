af7cca778d65067f4bf2ffb45b11f59abd78ad01
Justine <justine.trupheme@epitech.eu>
2022-10-31 13:41:34 +0100

    change: fix scroll in modal, add multiples type of close in modal, begin to css everything'

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import './App.scss';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Login from './view/Login';
-import Register from './view/Register';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Login from "./view/Login";
+import Register from "./view/Register";
 import Footer from './components/Footer';
-import Home from './view/Home';
-import AreaPage from './view/Area';
-import Services from './view/Services';
+import Home from "./view/Home";
+import AreaPage from "./view/Area";
+import Services from "./view/Services";
 
 function App() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/areas" element={<AreaPage />} />
-                    <Route path="/services" element={<Services />} />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </div>
-    );
+  
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/areas" element={<AreaPage />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
