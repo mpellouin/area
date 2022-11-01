22d9d5e408e22a9ded126fa740ace490ac90abaa
Laurent-cazette <Laurent.cazette@epitech.eu>
2022-11-01 21:04:12 +0100

    fixed non working auth buttons

--- mobile/src/view/signFiles/auth.tsx
+++ mobile/src/view/signFiles/auth.tsx
@@ -1,49 +1,50 @@
-import React from 'react';
-import {View, Linking, Button, Alert} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import { WebView } from "react-native-webview";
+import React, { useState, useEffect } from "react";
+import { View, Linking, Button, Alert} from "react-native";
+import {Colors} from '../../../Style';
+import { useNavigation } from "@react-navigation/native";
 
 const Auth = () => {
-  const navigation = useNavigation();
+    const navigation = useNavigation()
 
-  Linking.addEventListener('url', handleUrl);
+    Linking.addEventListener('url', handleUrl)
 
-  async function handleUrl(event: {url: string; param: string}) {
-    try {
-      const accessToken = await getSearchParamFromURL(event.url, 'token');
-      if (accessToken != null) {
-        navigation.navigate('User');
-        Linking.removeAllListeners('url');
-      } else {
-        Alert.alert('An error occured while signin you');
-      }
-    } catch (err) {
-      throw 'An error occured: ' + err;
-    }
-  }
-
-  const openBrowser = (url: string) => {
-    Linking.openURL(url);
-  };
-
-  const getSearchParamFromURL = (url: string, param: string) => {
-    const include = url.includes(param);
-    if (!include) return null;
-    const params = url.split(/([?,=])/);
-    const index = params.indexOf(param);
-    const value = params[index + 2];
-    return value;
-  };
-
-  return (
-    <View>
-      <Button
-        title="google"
-        onPress={() =>
-          openBrowser('http://area.eu-west-3.elasticbeanstalk.com/auth/google')
+    async function handleUrl(event: any) {
+        try {
+            const accessToken = await getSearchParamFromURL(event.url, "token")
+            if(accessToken != null) {
+                console.log(accessToken)
+                navigation.navigate('User')
+                Linking.removeAllListeners('url')
+            } else {
+                Alert.alert("An error occured while signin you")
+            }
+        } catch (err) {
+            throw("An error occured: " + err)
         }
-      />
-    </View>
-  );
-};
+    }
+
+    const openBrowser = (url: string) => {
+        Linking.openURL(url)
+    };
+
+    const getSearchParamFromURL = (url: string, param: string) => {
+        const include = url.includes(param)
+        if (!include) return null
+        const params = url.split(/([?,=])/)
+        const index = params.indexOf(param)
+        const value = params[index + 2]
+        return value
+    }
+
+    return (
+        <View>
+            <Button
+                title="google"
+                onPress={() =>  openBrowser("http://area.eu-west-3.elasticbeanstalk.com/auth/google")}
+            />
+        </View>
+    )
+}
 
 export default Auth;
