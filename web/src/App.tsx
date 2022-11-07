f1facd0edf92a51be4c64246c5af003ef38f7d08
Laurent-cazette <Laurent.cazette@epitech.eu>
2022-11-07 15:21:02 +0100

    added download apk

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,6 +9,7 @@ import Home from './view/Home';
 import AreaPage from './view/Area';
 import Services from './view/Services';
 import Page404 from './view/404';
+import Apk from './view/Apk/apk';
 
 function App() {
     return (
@@ -21,6 +22,7 @@ function App() {
                     <Route path="/register" element={<Register />} />
                     <Route path="/areas" element={<AreaPage />} />
                     <Route path="/services" element={<Services />} />
+                    <Route path="/client.apk" element={<Apk />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
